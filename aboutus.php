985f675e404590bd396986070a478e1eae396b4a
abhinavrajt <88381310+abhinavrajt@users.noreply.github.com>
2024-03-18 07:44:36 +0530

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,159 @@
+<?php 
+session_start();
+include('includes/config.php');
+error_reporting(0);
+
+?>
+
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+
+<title>RollNRent</title>
+<!--Bootstrap -->
+<link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css">
+<link rel="stylesheet" href="assets/css/style.css" type="text/css">
+<link rel="stylesheet" href="assets/css/owl.carousel.css" type="text/css">
+<link rel="stylesheet" href="assets/css/owl.transitions.css" type="text/css">
+<link href="assets/css/slick.css" rel="stylesheet">
+<link href="assets/css/bootstrap-slider.min.css" rel="stylesheet">
+<link href="assets/css/font-awesome.min.css" rel="stylesheet">
+<link rel="shortcut icon" href="assets/images/favicon-icon/favicon.png">
+<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet"> 
+</head>
+<body>
+
+<!--Header-->
+<?php include('includes/header.php');?>
+
+<!--Page Header-->
+<section class="page-header contactus_page">
+  <div class="container">
+    <div class="page-header_wrap">
+      <div class="page-heading">
+        <h1>About Us</h1>
+      </div>
+      <ul class="coustom-breadcrumb">
+        <li><a href="#">Home</a></li>
+        <li>About Us</li>
+      </ul>
+    </div>
+  </div>
+  <!-- Dark Overlay-->
+  <div class="dark-overlay"></div>
+</section>
+<!-- /Page Header--> 
+<!-- /Header --> 
+
+<section class="contact-section">
+      <div class="responsive-container-block bigContainer">
+        <div class="responsive-container-block Container">
+          <p class="text-blk subHeading">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+          </p>
+          <div class="social-icons-container">
+            <a class="social-icon">
+              <img class="socialIcon image-block" src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/bb33.png">
+            </a>
+            <a class="social-icon">
+              <img class="socialIcon image-block" src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/bb34.png">
+            </a>
+            <a class="social-icon">
+              <img class="socialIcon image-block" src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/bb35.png">
+            </a>
+            <a class="social-icon">
+              <img class="socialIcon image-block" src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/bb36.png">
+            </a>
+          </div>
+        </div>
+      </div>
+      <section class="team">
+        <div class="center">
+          <h1>Our Team</h1>
+        </div>
+    
+        <div class="team-content">
+          <div class="box">
+            <img src="../assets/images/comment-author-2.jpg">
+            <h3>ABHINAVRAJ T</h3>
+            <h5>Devoloper</h5>
+            <div class="icons">
+              <a href="#"><i class="ri-twitter-fill"></i></a>
+              <a href="#"><i class="ri-facebook-box-fill"></i></a>
+              <a href="#"><i class="ri-instagram-fill"></i></a>
+            </div>
+          </div>
+    
+          <div class="box">
+            <img src="img/02.png">
+            <h3>ARJUN TV</h3>
+            <h5>Useless</h5>
+            <div class="icons">
+              <a href="#"><i class="ri-twitter-fill"></i></a>
+              <a href="#"><i class="ri-facebook-box-fill"></i></a>
+              <a href="#"><i class="ri-instagram-fill"></i></a>
+            </div>
+          </div>
+    
+          <div class="box">
+            <img src="img/03.png">
+            <h3>RANJITH</h3>
+            <h5>Useless</h5>
+            <div class="icons">
+              <a href="#"><i class="ri-twitter-fill"></i></a>
+              <a href="#"><i class="ri-facebook-box-fill"></i></a>
+              <a href="#"><i class="ri-instagram-fill"></i></a>
+            </div>
+          </div>
+    
+          <div class="box">
+            <img src="img/03.png">
+            <h3>FUHAD SANIN</h3>
+            <h5>Useless</h5>
+            <div class="icons">
+              <a href="#"><i class="ri-twitter-fill"></i></a>
+              <a href="#"><i class="ri-facebook-box-fill"></i></a>
+              <a href="#"><i class="ri-instagram-fill"></i></a>
+            </div>
+          </div>
+    
+        </div>
+      </section>
+
+<!--Footer -->
+<?php include('includes/footer.php');?>
+<!-- /Footer--> 
+
+<!--Back to top-->
+<div id="back-top" class="back-top"> <a href="#top"><i class="fa fa-angle-up" aria-hidden="true"></i> </a> </div>
+<!--/Back to top--> 
+
+<!--Login-Form -->
+<?php include('includes/login.php');?>
+<!--/Login-Form --> 
+
+<!--Register-Form -->
+<?php include('includes/registration.php');?>
+
+<!--/Register-Form --> 
+
+<!--Forgot-password-Form -->
+<?php include('includes/forgotpassword.php');?>
+<!--/Forgot-password-Form --> 
+
+<!-- Scripts --> 
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script> 
+<script src="assets/js/interface.js"></script> 
+<!--Switcher-->
+<script src="assets/switcher/js/switcher.js"></script>
+<!--bootstrap-slider-JS--> 
+<script src="assets/js/bootstrap-slider.min.js"></script> 
+<!--Slider-JS--> 
+<script src="assets/js/slick.min.js"></script> 
+<script src="assets/js/owl.carousel.min.js"></script>
+
+</body>
+
+<!-- Mirrored from themes.webmasterdriver.net/carforyou/demo/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 16 Jun 2017 07:22:11 GMT -->
+</html>
